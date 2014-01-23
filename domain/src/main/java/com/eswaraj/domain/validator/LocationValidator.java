c03757a1d33d6ba9e7972a6e03155a5a8feb2807
aannuujj <aannuujj@users.noreply.github.com>
2014-01-23 22:26:30 +0000

    corrected locationvalidator

--- domain/src/main/java/com/eswaraj/domain/validator/LocationValidator.java
+++ domain/src/main/java/com/eswaraj/domain/validator/LocationValidator.java
@@ -3,22 +3,22 @@ package com.eswaraj.domain.validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.eswaraj.domain.nodes.Person;
+import com.eswaraj.domain.nodes.Location;
 import com.eswaraj.domain.validator.exception.ValidationException;
 
 @Component
-public class LocationValidator extends BaseValidator<Person>{
+public class LocationValidator extends BaseValidator<Location>{
 
 	@Autowired
 	public LocationValidator(ValidationManager validationManager) {
-		super(Person.class, validationManager);
+		super(Location.class, validationManager);
 	}
 
-	public void validateBeforeSave(Person person) throws ValidationException {
+	public void validateBeforeSave(Location location) throws ValidationException {
 		
 	}
 
-	public void validateBeforeDelete(Person person) throws ValidationException {
+	public void validateBeforeDelete(Location location) throws ValidationException {
 		
 	}
 }
