7c3d4037e7836468d3179641e25fd0b2500ffa4c
Ravi Sharma <ping2ravi@gmail.com>
2014-05-25 18:00:14 +0100

    Web test cases fixed

--- web/src/test/java/com/eswaraj/web/admin/controller/MockServiceConfig.java
+++ web/src/test/java/com/eswaraj/web/admin/controller/MockServiceConfig.java
@@ -4,6 +4,8 @@ import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.eswaraj.core.service.CustomService;
+import com.eswaraj.core.service.FileService;
 import com.eswaraj.core.service.LocationService;
 
 @Configuration
@@ -13,5 +15,14 @@ public class MockServiceConfig {
 	public LocationService locationService() {
 		return Mockito.mock(LocationService.class);
 	}
+	
+	@Bean
+	public CustomService customService() {
+		return Mockito.mock(CustomService.class);
+	}
 
+	@Bean
+	public FileService fileService() {
+		return Mockito.mock(FileService.class);
+	}
 }
